502c801487c8d58609400c05998f3101fc4446b5
JmcaDev <jmcastilloabreu@gmail.com>
2026-07-17 17:05:27 -0400

    Axios, types & layouts

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,4 +1,5 @@
 import { Outlet } from 'react-router-dom'
+import { Toaster } from 'sonner'
 
 function AuthLayout() {
   return (
@@ -12,6 +13,8 @@ function AuthLayout() {
           </div>
         </div>
       </div>
+
+      <Toaster position='top-right'/>
     </>
   )
 }
